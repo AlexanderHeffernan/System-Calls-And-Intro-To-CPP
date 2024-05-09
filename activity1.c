081fd0062686519ea823e40e404ce8fe632d375a
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-05-09 18:54:16 +1200

    Implemented codes for the pid cases to complete activity1

--- activity1.c
+++ activity1.c
@@ -11,11 +11,24 @@ int main()
 
     switch(pid){
         case -1:
-            /* code for case -1 */
+            printf("Error\n");
+            exit(1);
 
         case 0:
-            /* code for case 0 */
+            if (execl("/bin/ps", "ps", "-A", NULL) == -1) {
+                perror("Error executing exec");
+                exit(1);
+            }
         default:
-            /* code for case default */
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status)) {
+                printf("Parent Process ID: %d\n", getpid());
+                printf("Child Process ID: %d\n", pid);
+                printf("Child Termination Status: %d\n", WEXITSTATUS(status));
+            }
     }
+
+    return 0;
 }
